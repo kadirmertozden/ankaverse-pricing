3962c5c3ac9e4d132e7ba205748dcaa948b2c0b7
Kadir Mert Ozden <kadirmertozden@gmail.com>
2025-08-09 11:48:44 +0000

    Enable API routing (Laravel 12) + pricing endpoints

--- app/Http/Controllers/PriceController.php
+++ app/Http/Controllers/PriceController.php
@@ -31,21 +31,40 @@ class PriceController extends Controller
         return response()->json($svc->compute($p, $opt));
     }
 
-    public function products()
-    {
-        // Şimdilik dummy; sonra XML okuyup gerçek ürün listesi döneceğiz
-        return response()->json([
-            ['stock_code' => 'P11743S4450', 'name' => '4 Shot Bardaklı Matara Seti'],
-        ]);
+public function products()
+{
+    return response()->json([
+        ['stockCode' => 'P11743S4450', 'name' => 'Ürün 1', 'price' => 100],
+        ['stockCode' => 'P11856S4460', 'name' => 'Ürün 2', 'price' => 150],
+    ]);
+}
+
+public function compute(Request $request)
+{
+    $data = $request->validate([
+        'stockCode' => 'required|string',
+        'buyPriceVat' => 'required|numeric',
+        'commissionRate' => 'required|numeric',
+        'width' => 'required|numeric',
+        'length' => 'required|numeric',
+        'height' => 'required|numeric',
+        'profit.mode' => 'required|string',
+        'profit.value' => 'required|numeric',
+        'freeShipThreshold' => 'required|numeric',
+    ]);
+
+    // Hesaplama örneği
+    $price = $data['buyPriceVat'] + ($data['buyPriceVat'] * $data['commissionRate'] / 100);
+    if ($data['profit']['mode'] === 'percent') {
+        $price += ($price * $data['profit']['value'] / 100);
+    } else {
+        $price += $data['profit']['value'];
     }
 
-    private function arasTable(): array
-    {
-        // 0..15 desi örnek tablo (gerçekte DB/ayar dosyasından gelecek)
-        return [
-            0=>66.79,1=>66.79,2=>73.48,3=>80.28,4=>88.55,
-            5=>96.63,6=>103.12,7=>109.62,8=>116.10,9=>122.60,
-            10=>129.10,11=>134.30,12=>139.53,13=>144.73,14=>149.94,15=>155.16,
-        ];
-    }
+    return response()->json([
+        'stockCode' => $data['stockCode'],
+        'calculatedPrice' => round($price, 2),
+    ]);
+}
+
 }
