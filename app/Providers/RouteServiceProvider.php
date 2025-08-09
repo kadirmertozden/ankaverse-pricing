52c6b7fba15c06ea84ca4aa6d9800559a746a260
Kadir Mert Ozden <kadirmertozden@gmail.com>
2025-08-09 12:46:59 +0000

    add products table migration

---
+++ app/Providers/RouteServiceProvider.php
@@ -0,0 +1,9 @@
+<?php
+
+protected function mapApiRoutes()
+{
+    Route::prefix('api')
+        ->middleware('api')
+        ->namespace($this->namespace)
+        ->group(base_path('routes/api.php'));
+}
