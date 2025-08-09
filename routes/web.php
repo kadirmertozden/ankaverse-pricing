38d7de6083c335ca101f36230d25a404284b0f0d
Kadir Mert Ozden <kadirmertozden@gmail.com>
2025-08-09 11:04:45 +0000

    Fix duplicate Route import and add /health

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-use Illuminate\Support\Facades\Route;
-
 Route::get('/health', function () {
     return response()->json([
         'ok' => true,
