290bb06374d2fe091866dbba7ae765e481215923
Kadir Mert Ozden <kadirmertozden@gmail.com>
2025-08-11 15:02:15 +0000

    Fix: implement FilamentUser and canAccessPanel() for panel authorization

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,24 +6,34 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+// EKLE
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
+
+class User extends Authenticatable implements FilamentUser
 {
     use HasFactory, Notifiable;
 
     protected $fillable = [
-        'name',
-        'email',
-        'password',
-        'is_admin',
+        'name', 'email', 'password', 'is_admin',
     ];
 
     protected $hidden = [
-        'password',
-        'remember_token',
+        'password', 'remember_token',
     ];
 
     protected $casts = [
         'email_verified_at' => 'datetime',
         'is_admin' => 'boolean',
     ];
+
+    // EKLE: Panel erişim kuralı
+    public function canAccessPanel(Panel $panel): bool
+    {
+        // Sadece adminler girsin istiyorsan:
+        return (bool) $this->is_admin;
+
+        // Geçici olarak herkes girsin istersen:
+        // return true;
+    }
 }
